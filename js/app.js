53097a065bdd550c1c1d73262b177db4c89c5889
russvetsper <russvetsper@gmail.com>
2017-01-13 06:35:21 -0800

    first commit

--- js/app.js
+++ js/app.js
@@ -12,11 +12,11 @@ particlesJS.load('particles-js', 'particles.json', function() {
 /* Otherwise just put the config content (json): */
 
 particlesJS('particles-js',
-  
+
   {
     "particles": {
       "number": {
-        "value": 80,
+        "value": 100,
         "density": {
           "enable": true,
           "value_area": 800
